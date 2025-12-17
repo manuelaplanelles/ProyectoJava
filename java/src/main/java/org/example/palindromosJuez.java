cb8cd13e5fce919fffc95e0e371541d96ced7580
MANUELA\manue <manuelaplanelles@gmail.com>
2025-12-17 01:08:06 +0100

    se cambia de proyecto, y empezamos con el codigo del juez

---
+++ java/src/main/java/org/example/palindromosJuez.java
@@ -0,0 +1,24 @@
+public class palindromosJuez {
+    // Asume fichero llamado solution.java
+    //public class solution {
+
+        static java.util.Scanner in;
+
+        public static boolean casoDePrueba() {
+            leer caso de prueba
+            if (es el caso que marca el final)
+            return false;
+        else {
+                // CÓDIGO PRINCIPAL AQUÍ
+                return true;
+            }
+        } // casoDePrueba
+
+        public static void main(String[] args) {
+            in = new java.util.Scanner(System.in);
+            while (casoDePrueba()) {
+            }
+        } // main
+
+    } // class solution
+}
