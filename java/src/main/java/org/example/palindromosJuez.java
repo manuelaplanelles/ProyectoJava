66781efcbd51d8a46bd75a2cf5f3a5b68299e7e5
MANUELA\manue <manuelaplanelles@gmail.com>
2025-12-18 15:15:06 +0100

    corregimos ejercicio para el juez

--- java/src/main/java/org/example/palindromosJuez.java
+++ java/src/main/java/org/example/palindromosJuez.java
@@ -1,46 +1,37 @@
 import java.util.Scanner;
-
 public class palindromosJuez {
-
-        static java.util.Scanner in;
-
-        public static boolean casoDePrueba() {
-            Scanner teclado = new Scanner(System.in);
-           String frase;
-            boolean igual = true;
-
-                System.out.println("Escribe tu frase: ");
-                frase = teclado.nextLine();
-            if (frase.equalsIgnoreCase("XXX")){
-                return false;
-            }else {
-                int j=frase.length()-1;
-                igual=true;
-                int i=0;
-                  while (i<j && igual){
-                      while (i < j && frase.charAt(i) == ' ') i++;
-                      while (i < j && frase.charAt(j) == ' ') j--;
-                      System.out.println(frase.charAt(i) + " " + frase.charAt(j));
-                      if (Character.toLowerCase(frase.charAt(i))==Character.toLowerCase(frase.charAt(j))){
-                          j--;
-                            i++;
-                      }else {
-                          igual = false;
-                      }
-                  }
-
-                  if(igual){
-                      System.out.println("Son palindromos.");
-                  }else{
-                      System.out.println("No son palindromos.");
-                  }
+    static java.util.Scanner in;
+    public static boolean casoDePrueba() {
+        boolean igual = true;
+        //System.out.println("Escribe tu frase: ");
+        String frase = in.nextLine();
+        if (frase.equals("XXX")){
+            return false;
+        }else {
+            int i = 0;
+            int j = frase.length() - 1;
+            while (i < j && igual) {
+                while (i < j && frase.charAt(i) == ' ') i++;
+                while (i < j && frase.charAt(j) == ' ') j--;
+                //System.out.println(frase.charAt(i) + " " + frase.charAt(j));
+                if (Character.toLowerCase(frase.charAt(i)) == Character.toLowerCase(frase.charAt(j))) {
+                    j--;
+                    i++;
+                } else {
+                    igual = false;
                 }
-
-            return true;
-        }
-        public static void main(String[] args) {
-            in = new java.util.Scanner(System.in);
-            while (casoDePrueba()) {
+            }
+            if (igual) {
+                System.out.println("SI");
+            } else {
+                System.out.println("NO");
             }
         }
+        return true;
     }
+    public static void main(String[] args) {
+        in = new java.util.Scanner(System.in);
+        while (casoDePrueba()) {
+        }
+    }
+}
