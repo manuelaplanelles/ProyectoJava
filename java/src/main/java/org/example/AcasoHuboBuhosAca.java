12c750d56451a9c6d7dd137f35b83336c09583cf
MANUELA\manue <manuelaplanelles@gmail.com>
2025-12-18 16:34:40 +0100

    creo mi palindromo y el proyecto bonito

--- java/src/main/java/org/example/AcasoHuboBuhosAca.java
+++ java/src/main/java/org/example/AcasoHuboBuhosAca.java
@@ -1,40 +1,50 @@
 import java.util.Scanner;
 
 public class AcasoHuboBuhosAca {
-    public static void main(String[]args) {
+
+    static java.util.Scanner in;
+
+    public static String escribe () {
         Scanner teclado = new Scanner(System.in);
+        String frase;
+        System.out.println("Escribe tu frase: ");
+        frase = teclado.nextLine();
+        return frase;
+    }
+    public static boolean casoDePrueba() {
+        boolean igual = true;
+        String frase = escribe();
 
-        System.out.println("--- DETECTOR DE PALINDROMOS ---");
-        System.out.println("Escribe XXX para terminar");
-
-        while (true){
-            System.out.println("Introduce tu frase: ");
-            String frase = teclado.nextLine();
-
-            if (frase.equalsIgnoreCase("XXX")){
-                break;
-            }
-            int longitud = frase.length();
-
-            if (longitud == 0 || longitud > 100) {
-                System.out.println("Error: La frase debe tener entre 1 y 100 caracteres.");
-                continue;
-            }
-
-            if (frase.charAt(0) == ' ' || frase.charAt(longitud - 1) == ' ') {
-                System.out.println("Error: La frase no puede empezar ni terminar con espacios.");
-                continue;
-            }
-            boolean cartInv = false;
-
-            for (int i = 0; i<longitud; i++){
-                char cart = frase.charAt(i);
-                if (!((cart >= 'a' && cart <= 'z') || (cart >= 'A' && cart <= 'Z') || cart == ' ')) {
-                    System.out.println("Error: Solo letras inglesas y espacios.");
-                    cartInv = true;
-                    break;
+        if (frase.equalsIgnoreCase("XXX")){
+            return false;
+        }else {
+            int j=frase.length()-1;
+            igual=true;
+            int i=0;
+            while (i<j && igual){
+                while (i < j && frase.charAt(i) == ' ') i++;
+                while (i < j && frase.charAt(j) == ' ') j--;
+                System.out.println(frase.charAt(i) + " " + frase.charAt(j));
+                if (Character.toLowerCase(frase.charAt(i))==Character.toLowerCase(frase.charAt(j))){
+                    j--;
+                    i++;
+                }else {
+                    igual = false;
                 }
             }
+
+            if(igual){
+                System.out.println("Son palindromos.");
+            }else{
+                System.out.println("No son palindromos.");
+            }
+        }
+
+        return true;
+    }
+    public static void main(String[] args) {
+        in = new java.util.Scanner(System.in);
+        while (casoDePrueba()) {
         }
     }
 }
